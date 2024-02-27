d364bbdb2dd0146ca76d49341f9747fe0767050f
Costinel22 <malaiasi_costinel@yahoo.com>
2024-02-27 10:58:15 +0000

    add the questions to the Engineer, Intern and Manager on the index.js

--- index.js
+++ index.js
@@ -11,3 +11,66 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+
+
+
+const setEnginner = [{
+        type: "input",
+        name: "enginnerName",
+        message: "Please enter the name of the enginner!"
+    },
+    {
+        type: "input",
+        name: "enginnerId",
+        message: "Please enter the Id of the enginner!"
+    }, {
+        type: "input",
+        name: "enginnerEmail",
+        message: "Please enter the email of the enginner!"
+    }, {
+        type: "input",
+        name: "enginnerGithub",
+        message: "Please enter the Github of the enginner!"
+    }
+]
+
+const setIntern = [{
+        type: "input",
+        name: "internName",
+        message: "Please enter the name of the intern!"
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "Please enter the Id of the intern!"
+    }, {
+        type: "input",
+        name: "internEmail",
+        message: "Please enter the email of the intern!"
+    }, {
+        type: "input",
+        name: "internSchool",
+        message: "Please enter the School of the intern!"
+    }
+]
+
+const setManager = [{
+        type: "input",
+        name: "managerName",
+        message: "Please enter the name of the manager!"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "Please enter the Id of the manager!"
+    }, {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter the email of the manager!"
+    }, {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Please enter the officeNumber of the manager!"
+    }
+]
